1012c115e3fceaf79da7ff034a15feb4d67d7b49
mohdabdin <72082608+mohdabdin@users.noreply.github.com>
2021-01-20 17:19:12 +0200

    Update Visualization.py

--- Visualization.py
+++ Visualization.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import style
 
-
+#changes the matplotlib's figure to black (optional)
 style.use('dark_background')
 
 
@@ -20,23 +20,27 @@ class TradingGraph:
         plt.show()
         
     def plot_candles(self, ax, ohlc, idx):
+        #iterate over each ohlc value along with the index
        for row, ix in zip(ohlc, idx):
            if row[3]<row[0]:
                clr = "red"
            else:
                clr = "green"
 
+           #plots a thin line to represent high and low values 
            ax.plot([ix, ix], [row[1], row[2]], lw=1, color=clr)
+           #plot a wider line to represent open and close
            ax.plot([ix, ix], [row[3], row[0]], lw=3, color=clr)
            
                 
     def render_prices(self, current_step, lbw):     
         for splot, df in zip(self.axs.flatten(), self.dfs):
             splot.clear()
-            #Format data for OHCL candlestick graph
+            
             step_range = range(current_step-lbw, current_step)
             idx = np.array(step_range)
             
+            #prepare a 3-d numpy array to be used by our plot_candles function
             candlesticks = zip(df['open'].values[step_range], 
                                df['high'].values[step_range],
                                df['low'].values[step_range], 
@@ -50,6 +54,7 @@ class TradingGraph:
         for splot, coin in zip(self.axs.flatten(), trades):
             for trade in coin:
                 if current_step>trade[1]>current_step-lbw:
+                    #plot the point at which the trade happened
                     if trade[0]=='buy':
                         clr = 'red'
                         splot.plot(trade[1], trade[2], 'ro')
@@ -57,15 +62,18 @@ class TradingGraph:
                         clr = 'green'
                         splot.plot(trade[1], trade[2], 'go')
                 
+                #the plotted dot won't appear after the look back window is passed so a horizontal line keeps tracks at any time
                 splot.hlines(trade[2], current_step-lbw, current_step, linestyle='dashed', colors=[clr])
 
     def render(self, current_step, window_size, trades):
         self.render_prices(current_step, window_size)
         self.render_trades(current_step, window_size, trades)
 
-        
+        #the draw function redraws the figure after all plots have been executed
         self.fig.canvas.draw() 
         self.fig.canvas.flush_events()
+        
+        #the pause is necessary to view the frame and allow the live updating mechanism on our figure
         plt.pause(0.1)
 
     def close(self):
